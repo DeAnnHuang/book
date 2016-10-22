9c501597097da63daa369c26234f2012c332623a
Fine <b10030007@gmail.com>
2016-10-22 20:09:47 +0800

    commit

--- assets/js/section.js
+++ assets/js/section.js
@@ -7,7 +7,15 @@ $(document).ready(function() {
     //Section
     var sectionTl = new TimelineMax();
     sectionTl
-        .fromTo('.bg.two', 1, { y: "100%" }, { y: "0%", ease: Linear.easeNone })
+        .to('.door', 1, { rotationY: '-45deg' })
+        .to('.pencilMan', 0.6, { left: '16vw', autoAlpha: 1, ease: Power2.easeIn }, 0.3)
+        .staggerTo('.text', 1.5, { marginLeft: '-100px', autoAlpha: 0, ease: Power2.easeIn }, -0.15)
+        .to('.gift', 0.8, { left: '59vw', autoAlpha: 0, ease: Power2.easeIn }, '-=0.9')
+        .to('.blur', 1, { left: '30vw', autoAlpha: 0, ease: Power2.easeIn }, '-=0.6')
+        .to('.pencilMan', 0.6, { left: '9vw', autoAlpha: 0, ease: Power2.easeIn }, '-=0.3')
+        .to('.door', 1, { rotationY: '0' })
+        .to('.main', 2, { left: '40vw' }, '-=1')
+        .fromTo('.bg.two', 1, { y: "100%" }, { y: "0%", ease: Linear.easeNone }, '-=0.8')
         .fromTo('.bg.three', 1, { y: "100%" }, { y: "0%", ease: Linear.easeNone })
         .fromTo('.bg.four', 1, { y: "100%" }, { y: "0%", ease: Linear.easeNone })
         .fromTo('.bg.five', 1, { y: "100%" }, { y: "0%", ease: Linear.easeNone });
--- build/js/all.js
+++ build/js/all.js
@@ -100,6 +100,7 @@ $(document).ready(function() {
 
     });
 
+
 });
 
 $(document).ready(function() {
@@ -108,10 +109,32 @@ $(document).ready(function() {
     var controller = new ScrollMagic.Controller();
 
 
+    //0
+    var girlPath = {
+        curviness: 1.25,
+        // autoRotate: true,
+        values: [
+            { top: '6vh', left: '15vw' },
+
+            { top: '10vh', left: '30vw' },
+            { top: '5vh', left: '55vw' },
+            { top: '-100%', left: '80vw' }
+        ]
+    }
+
+
     //Section
     var sectionTl = new TimelineMax();
     sectionTl
-        .fromTo('.bg.two', 1, { y: "100%" }, { y: "0%", ease: Linear.easeNone })
+        .to('.door', 1, { rotationY: '-45deg' })
+        .to('.pencilMan', 0.6, { left: '16vw', autoAlpha: 1, ease: Power2.easeIn }, 0.3)
+        .staggerTo('.text', 1.5, { marginLeft: '-100px', autoAlpha: 0, ease: Power2.easeIn }, -0.15)
+        .to('.gift', 0.8, { left: '59vw', autoAlpha: 0, ease: Power2.easeIn }, '-=0.9')
+        .to('.blur', 1, { left: '30vw', autoAlpha: 0, ease: Power2.easeIn }, '-=0.6')
+        .to('.pencilMan', 0.6, { left: '9vw', autoAlpha: 0, ease: Power2.easeIn }, '-=0.3')
+        .to('.door', 1, { rotationY: '0' })
+        .to('.main', 2, { bezier: girlPath, ease: Power2.easeIn }, '-=1')
+        .fromTo('.bg.two', 1, { y: "100%" }, { y: "0%", ease: Linear.easeNone }, '-=1')
         .fromTo('.bg.three', 1, { y: "100%" }, { y: "0%", ease: Linear.easeNone })
         .fromTo('.bg.four', 1, { y: "100%" }, { y: "0%", ease: Linear.easeNone })
         .fromTo('.bg.five', 1, { y: "100%" }, { y: "0%", ease: Linear.easeNone });
